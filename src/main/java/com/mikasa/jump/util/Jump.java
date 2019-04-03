db25747bb33ed5e9cdd81f4e14bf30bb0a9fe625
mikasa007 <17863109506@163.com>
2019-04-03 12:54:55 +0800

    微信跳一跳增加readme和建立工具类

---
+++ src/main/java/com/mikasa/jump/util/Jump.java
@@ -0,0 +1,15 @@
+package com.mikasa.jump.util;
+
+/**
+ * @author tianluhua
+ * @ClassName：Jump
+ * @description TODO(微信跳一跳手动版)
+ * 如果注释太多，可以用这个外连接的方式，写注释文档
+ * <a href="http://www.baidu.com" value="百度一下"></a>
+ * @date 2019/4/3 12:47
+ */
+public class Jump {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
